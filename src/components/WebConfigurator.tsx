b015d0b9a24238b3196e390ac0309cc365c961b6
edgar muñoz manjón <edgarmunozmanjon@gmail.com>
2025-05-05 10:36:38 +0200

    Exercice 3 finished with buttons added

--- src/components/WebConfigurator.tsx
+++ src/components/WebConfigurator.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { WebConfig } from '../types'
+import { WebConfig } from '../types/index'
 
 interface Props {
   config: WebConfig
@@ -8,28 +8,38 @@ interface Props {
 
 const WebConfigurator: React.FC<Props> = ({ config, onChange }) => {
   const handleChange = (field: keyof WebConfig, value: number) => {
-    onChange({ ...config, [field]: value })
+    const valid = Math.max(0, value)
+    onChange({ ...config, [field]: valid })
   }
 
   return (
     <div className="web-config">
       <label>
         Nombre de pàgines
-        <input
-          type="number"
-          min={0}
-          value={config.pages}
-          onChange={e => handleChange('pages', Number(e.target.value))}
-        />
+        <div className="input-group">
+          <button onClick={() => handleChange('pages', config.pages - 1)}>-</button>
+          <input
+            type="number"
+            min={0}
+            value={config.pages}
+            onChange={e => handleChange('pages', Number(e.target.value))}
+          />
+          <button onClick={() => handleChange('pages', config.pages + 1)}>+</button>
+        </div>
       </label>
+
       <label>
         Nombre de llenguatges
-        <input
-          type="number"
-          min={0}
-          value={config.languages}
-          onChange={e => handleChange('languages', Number(e.target.value))}
-        />
+        <div className="input-group">
+          <button onClick={() => handleChange('languages', config.languages - 1)}>-</button>
+          <input
+            type="number"
+            min={0}
+            value={config.languages}
+            onChange={e => handleChange('languages', Number(e.target.value))}
+          />
+          <button onClick={() => handleChange('languages', config.languages + 1)}>+</button>
+        </div>
       </label>
     </div>
   )
